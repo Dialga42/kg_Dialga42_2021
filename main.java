dfb403312f88de28400be78b3f217e98760746d4
Dialga42 <jiangdigong@gmail.com>
2020-03-13 20:59:35 -0400

    add judgement of args && refact hash1 to hash

--- main.java
+++ main.java
@@ -2,16 +2,20 @@ public class main {
 
     public static void main(String[] args) {
         // write your code here
+        if (args.length != 2) {
+            System.out.println("Invalid inout!");
+            return;
+        }
         String s1 = args[0], s2 = args[1];
         if (s1.length() != s2.length()) {
             System.out.println("false");
             return;
         }
-        char hash1[] = new char[128];
+        char hash[] = new char[128];
         for (int i = 0; i < s1.length(); i++) {
-            if (hash1[s1.charAt(i)] == 0) {
-                hash1[s1.charAt(i)] = s2.charAt(i);
-            } else if (hash1[s1.charAt(i)] == s2.charAt(i)) {
+            if (hash[s1.charAt(i)] == 0) {
+                hash[s1.charAt(i)] = s2.charAt(i);
+            } else if (hash[s1.charAt(i)] == s2.charAt(i)) {
                 continue;
             } else {
                 System.out.println("false");
